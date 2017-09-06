e5caae5c5949f557058a2da10c1b728fe3d5b5e6
freshield <zxdsw199182@gmail.com>
2017-09-06 16:41:51 -0400

    update

--- book/Chp1/src/RandomSeq.java
+++ book/Chp1/src/RandomSeq.java
@@ -1,2 +1,15 @@
+
+
 public class RandomSeq {
+
+    public static void main(String[] args) {
+        int N = 5;
+        double lo = 100.0;
+        double hi = 200.0;
+        for (int i = 0; i < N; i++) {
+            double x = StdRandom.uniform(lo,hi);
+            StdOut.printf("%.2f\n",x);
+        }
+    }
+
 }
