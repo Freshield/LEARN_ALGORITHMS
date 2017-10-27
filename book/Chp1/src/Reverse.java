f6a185674e6b7d4fedebc23715470c3f4cd91ddb
freshield <zxdsw199182@gmail.com>
2017-10-27 12:35:42 -0400

    update

--- book/Chp1/src/Reverse.java
+++ book/Chp1/src/Reverse.java
@@ -1,2 +1,16 @@
 public class Reverse {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        In in = new In("tinyT.txt");
+
+        while (!in.isEmpty()){
+            stack.push(in.readInt());
+        }
+
+        for (int i : stack){
+            System.out.println(i);
+        }
+    }
+
 }
