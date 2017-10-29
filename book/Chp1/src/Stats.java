8f1d54043996bdc8445d62fe562471cc7af33db5
freshield <zxdsw199182@gmail.com>
2017-10-29 12:30:07 -0400

    update

--- book/Chp1/src/Stats.java
+++ book/Chp1/src/Stats.java
@@ -1,2 +1,52 @@
 public class Stats {
+
+    public static void main(String[] args) {
+        Bag<Double> numbers = new Bag<Double>();
+
+        double[] temp = {100,99,101,120,98,107,109,81,101,90};
+
+        for (double t: temp
+             ) {
+            numbers.add(t);
+        }
+
+        int N = numbers.size();
+
+        double sum = 0.0;
+        for (double x : numbers){
+            sum += x;
+        }
+        double mean = sum / N;
+
+        sum = 0.0;
+        for (double x : numbers){
+            sum += (x - mean) * (x - mean);
+        }
+        double std = Math.sqrt(sum / (N - 1));
+
+        StdOut.printf("Mean: %.2f\n", mean);
+        StdOut.printf("Std dev: %.2f\n", std);
+
+        System.out.println("queue");
+        System.out.println();
+
+        In in = new In("tinyT.txt");
+        Queue<Integer> q = new Queue<Integer>();
+
+        while (!in.isEmpty()){
+            q.enqueue(in.readInt());
+        }
+
+        N = q.size();
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = q.dequeue();
+        }
+
+        for (int x : a){
+            System.out.println(x);
+        }
+
+    }
+
 }
