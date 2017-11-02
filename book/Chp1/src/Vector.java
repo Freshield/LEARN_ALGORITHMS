a4cbb5d20bb8c4a2f078b28fe28d530f92cf5d48
freshield <zxdsw199182@gmail.com>
2017-11-01 22:37:55 -0400

    update

--- book/Chp1/src/Vector.java
+++ book/Chp1/src/Vector.java
@@ -1,2 +1,24 @@
 public class Vector {
+
+    private final double[] coords;
+
+    public Vector(double[] a){
+        double[] copy = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            copy[i] = a[i];
+        }
+        coords = copy;
+    }
+
+    public double[] getCoords(){
+        return coords;
+    }
+
+    public static void main(String[] args) {
+        double[] a = {3.0, 4.0};
+        Vector vector = new Vector(a);
+        a[0] = 0.0;
+        System.out.println(vector.getCoords()[0]);
+    }
+
 }
