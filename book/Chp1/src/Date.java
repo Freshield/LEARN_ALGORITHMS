88bc3dd0e37bc677185c5c8ade378d3fb9ad1f50
freshield <zxdsw199182@gmail.com>
2017-11-12 19:26:18 -0500

    update

--- book/Chp1/src/Date.java
+++ book/Chp1/src/Date.java
@@ -1,2 +1,37 @@
 public class Date {
+
+    private final int month;
+    private final int day;
+    private final int year;
+
+    public Date(int m, int d, int y){
+        month = m;
+        day = d;
+        year = y;
+    }
+
+    public int month(){
+        return month;
+    }
+
+    public int day(){
+        return day;
+    }
+
+    public int year(){
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return month() + "/" + day() + "/" + year();
+    }
+
+    public static void main(String[] args) {
+        int m = 12;
+        int d = 31;
+        int y = 1999;
+        Date date = new Date(m,d,y);
+        System.out.println(date);
+    }
 }
