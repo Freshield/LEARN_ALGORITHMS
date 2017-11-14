6584e1ac985cd621280aeaf7c357287bd141b86f
freshield <zxdsw199182@gmail.com>
2017-11-14 17:30:22 -0500

    update

--- book/Chp1/src/Filps_1.java
+++ book/Chp1/src/Filps_1.java
@@ -1,2 +1,21 @@
 public class Filps_1 {
+
+    public static void main(String[] args) {
+        int T = 100000;
+
+        Counter_1 heads = new Counter_1("heads");
+        Counter_1 tails = new Counter_1("tails");
+        for (int i = 0; i < T; i++) {
+            if (StdRandom.bernoulli(0.5)){
+                heads.increment();
+            }else {
+                tails.increment();
+            }
+        }
+        System.out.println(heads);
+        System.out.println(tails);
+        int d = heads.tally() - tails.tally();
+        System.out.println("delta: " + Math.abs(d));
+    }
+
 }
