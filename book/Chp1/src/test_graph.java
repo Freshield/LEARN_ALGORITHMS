5f0c69edfa1115507cc1fecb247c5d86890df7b4
freshield <zxdsw199182@gmail.com>
2017-11-19 20:54:49 -0500

    update

--- book/Chp1/src/test_graph.java
+++ book/Chp1/src/test_graph.java
@@ -1,2 +1,31 @@
 public class test_graph {
+
+    public static void main(String[] args) {
+        double xlo = .2;
+        double xhi = .5;
+        double ylo = .5;
+        double yhi = .6;
+        int T = 10000;
+
+        Interval1D xinterval = new Interval1D(xlo, xhi);
+        Interval1D yinterval = new Interval1D(ylo, yhi);
+        Interval2D box = new Interval2D(xinterval, yinterval);
+        box.draw();
+
+        Counter c = new Counter("hits");
+        for (int i = 0; i < T; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            Point2D p = new Point2D(x, y);
+            if (box.contains(p)){
+                c.increment();
+            }else {
+                p.draw();
+            }
+        }
+
+        System.out.println(c);
+        System.out.println(box.area());
+    }
+
 }
