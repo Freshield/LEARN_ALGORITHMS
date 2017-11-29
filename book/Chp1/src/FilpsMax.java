df758789bc2587c2aa36b8d20d20ef04783597ac
freshield <zxdsw199182@gmail.com>
2017-11-28 20:57:14 -0500

    update

--- book/Chp1/src/FilpsMax.java
+++ book/Chp1/src/FilpsMax.java
@@ -1,2 +1,27 @@
 public class FilpsMax {
+
+    public static Counter max(Counter x, Counter y){
+        if (x.tally() > y.tally()) return x;
+        else return y;
+    }
+
+    public static void main(String[] args) {
+        int T = 100000;
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+        for (int i = 0; i < T; i++) {
+            if (StdRandom.bernoulli(0.5)){
+                heads.increment();
+            }else {
+                tails.increment();
+            }
+        }
+
+        if (heads.tally() == tails.tally()){
+            System.out.println("Tie");
+        }else {
+            System.out.println(max(heads,tails) + " wins");
+        }
+    }
+
 }
