1eaa294f8c84de83b3c804ce2a753524b5851312
freshield <zxdsw199182@gmail.com>
2017-11-29 20:54:25 -0500

    update

--- book/Chp1/src/testCounter.java
+++ book/Chp1/src/testCounter.java
@@ -1,11 +1,28 @@
 public class testCounter {
 
+    public static void increase(Counter test){
+        test.increment();
+    }
+
+    public static void increased(int a){
+        a++;
+    }
+
     public static void main(String[] args) {
         Counter heads = new Counter("heads");
         Counter tails = new Counter("tails");
 
         heads.increment();
-        System.out.println(heads.tally() - tails.tally());
+        //System.out.println(heads.tally() - tails.tally());
+        System.out.println(heads);
+        increase(heads);
+        System.out.println(heads);
+        int b = 1;
+        System.out.println(b);
+        increased(b);
+        System.out.println(b);
+
+
     }
 
 }
