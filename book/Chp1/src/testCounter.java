0345c704a1ff6d2282385eac475d5c96d3278109
freshield <zxdsw199182@gmail.com>
2017-12-01 20:40:32 -0500

    update

--- book/Chp1/src/testCounter.java
+++ book/Chp1/src/testCounter.java
@@ -1,5 +1,11 @@
 public class testCounter {
 
-    
+    public static void main(String[] args) {
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+
+        heads.increment();
+        System.out.println(heads.tally() - tails.tally());
+    }
 
 }
