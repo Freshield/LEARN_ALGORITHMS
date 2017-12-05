10a22c96803c79ae8dcb3de81ed40ca0c21b14d0
freshield <zxdsw199182@gmail.com>
2017-12-05 17:43:23 -0500

    update

---
+++ book/Chp1/src/BinarySearch.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        while (!Std)
+    }
+
+}
