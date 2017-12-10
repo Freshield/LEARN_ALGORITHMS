95148500c6635645860fc09239bfa08343780568
freshield <zxdsw199182@gmail.com>
2017-12-10 17:46:42 -0500

    update

--- book/Chp1/src/array_test.java
+++ book/Chp1/src/array_test.java
@@ -17,10 +17,23 @@ public class array_test {
         return sum / N;
     }
 
+    public static void seq_change(int[] input){
+        int N = input.length;
+        for (int i = 0; i < N/2; i++){
+            int temp = input[i];
+            input[i] = input[N-1-i];
+            input[N-i-1] = temp;
+        }
+    }
+
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5,6,7,8,9};
         System.out.println(max_num(a));
         System.out.println(average_num(a));
+        seq_change(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
 }
