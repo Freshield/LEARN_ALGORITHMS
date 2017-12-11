d5fff55aa4afb1c567e3b6e1381ca2da74363364
freshield <zxdsw199182@gmail.com>
2017-12-11 17:30:51 -0500

    update

--- book/Chp1/src/array_test.java
+++ book/Chp1/src/array_test.java
@@ -8,9 +8,19 @@ public class array_test {
         return max;
     }
 
+    public static double average_num(int[] input){
+        int N = input.length;
+        double sum = 0.0;
+        for (int i = 0; i < N; i++){
+            sum += input[i];
+        }
+        return sum / N;
+    }
+
     public static void main(String[] args) {
         int[] a = {1,2,3,4,5,6,7,8,9};
         System.out.println(max_num(a));
+        System.out.println(average_num(a));
     }
 
 }
