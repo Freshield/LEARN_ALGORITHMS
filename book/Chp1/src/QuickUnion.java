e4bdb92d978512b412832051657e65ff3576c088
freshield <zxdsw199182@gmail.com>
2017-12-25 16:13:26 -0500

    done quick union

---
+++ book/Chp1/src/QuickUnion.java
@@ -0,0 +1,62 @@
+public class QuickUnion {
+
+
+    private int[] id;
+
+    public QuickUnion(int num){
+        id = new int[num];
+        for (int i = 0; i < id.length; i++) {
+            id[i] = i;
+        }
+    }
+
+    public void showId(){
+        for (int i = 0; i < id.length; i++) {
+            System.out.print(id[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public int find(int p){
+        while (p != id[p]){
+            p = id[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q){
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q){
+        int pROOT = find(p);
+        int qROOT = find(q);
+
+        if (pROOT == qROOT) return;
+
+        id[pROOT] = qROOT;
+    }
+
+
+
+    public static void main(String[] args) {
+        String dir = "/media/freshield/COASAIR1/LEARN_ALGORITHMS/book/Chp1/src/";
+        String filename = "tinyUF.txt";
+        String[] orders = ReadFile.returnValue(dir, filename);
+        QuickUnion qu = new QuickUnion(Integer.parseInt(orders[0]));
+        qu.showId();
+        for (int i = 1; i < qu.id.length; i++) {
+            String order = orders[i];
+            System.out.println(order);
+            String[] temp = order.split(" ");
+            int p = Integer.parseInt(temp[0]);
+            int q = Integer.parseInt(temp[1]);
+            
+            qu.union(p, q);
+        }
+        qu.showId();
+
+    }
+
+
+}
