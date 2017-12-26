8019dedce91f3db1dbb60bde8893ad1a65da8488
freshield <zxdsw199182@gmail.com>
2017-12-26 16:44:47 -0500

    done insertion

--- book/Chp2/src/Insertion.java
+++ book/Chp2/src/Insertion.java
@@ -1,7 +1,12 @@
-public class Example {
+public class Insertion {
 
     public static void sort(Comparable[] a){
-
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
     }
 
     public static boolean less(Comparable v, Comparable w){
