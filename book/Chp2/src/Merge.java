8d96ca2cdb4ff49565f692485148a24282a526a5
freshield <zxdsw199182@gmail.com>
2017-12-27 19:54:34 -0500

    done Merge

--- book/Chp2/src/Merge.java
+++ book/Chp2/src/Merge.java
@@ -4,6 +4,15 @@ public class Merge {
 
     public static void sort(Comparable[] a){
         aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi){
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a,lo,mid);
+        sort(a,mid+1,hi);
+        merge(a,lo,mid,hi);
     }
 
     public static void merge(Comparable[] a, int lo, int mid, int hi){
@@ -41,7 +50,6 @@ public class Merge {
         int hi = test.length - 1;
         int mid = lo + (hi - lo) / 2;
         sort(test);
-        merge(test, lo, mid, hi);
         show(test);
     }
 
