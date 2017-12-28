9f8ca857559612ddf74622b6313de5a626f7bdd8
freshield <zxdsw199182@gmail.com>
2017-12-27 19:50:33 -0500

    begin Merge class

---
+++ book/Chp2/src/Merge.java
@@ -0,0 +1,49 @@
+public class Merge {
+
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a){
+        aux = new Comparable[a.length];
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi){
+        //copy the array
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k < hi + 1; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k < hi + 1; k++) {
+            if (i > mid)    a[k] = aux[j++];
+            else if (j > hi)    a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))  a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+
+    public static boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    public static void show(Comparable[] a){
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String a = "e e g m o r r s a e e l m p t x";
+        String[] test = a.split(" ");
+        show(test);
+        int lo = 0;
+        int hi = test.length - 1;
+        int mid = lo + (hi - lo) / 2;
+        sort(test);
+        merge(test, lo, mid, hi);
+        show(test);
+    }
+
+
+}
