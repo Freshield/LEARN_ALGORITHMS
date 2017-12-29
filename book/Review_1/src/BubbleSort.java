472b714cbe4ee6437546e2488b0ba202e7152961
freshield <zxdsw199182@gmail.com>
2017-12-29 16:35:10 -0500

    update

--- book/Review_1/src/BubbleSort.java
+++ book/Review_1/src/BubbleSort.java
@@ -1,2 +1,48 @@
 public class BubbleSort {
+
+
+    public static void sort(Comparable[] list){
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = list.length - 1; j > i; j--) {
+                if (less(list[j],list[j-1])){
+                    exch(j,j-1,list);
+                }
+            }
+        }
+    }
+
+    public static void exch(int a, int b, Comparable[] list){
+        Comparable temp = list[a];
+        list[a] = list[b];
+        list[b] = temp;
+    }
+
+    public static boolean less(Comparable a, Comparable b){
+        return a.compareTo(b) < 0;
+    }
+
+    public static boolean isSorted(Comparable[] list){
+        for (int i = 0; i < list.length - 1; i++) {
+            if (less(list[i+1], list[i]))   return false;
+        }
+        return true;
+    }
+
+    public static void show(Comparable[] list){
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String filename = "words3.txt";
+        String[] a = In.readStrings(filename);
+        String[] b = {"s","o","r","t","e","x","a","m","p","l","e"};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+
+
 }
