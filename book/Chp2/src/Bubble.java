1e21f83df4ee2e332ebd7b27951618085b84a940
freshield <zxdsw199182@gmail.com>
2018-01-04 20:35:28 -0500

    begin today

--- book/Chp2/src/Bubble.java
+++ book/Chp2/src/Bubble.java
@@ -3,8 +3,8 @@ public class Bubble {
     public static void sort(Comparable[] a){
         int N = a.length;
         for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1; j++) {
-                if (less(a[j+1], a[j])){
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (less(a[j+1],a[j])){
                     exch(a,j,j+1);
                 }
             }
